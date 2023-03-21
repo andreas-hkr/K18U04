784097462f57a843c66d75e8c1551f556130937a
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-21 14:51:33 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Beräkna längden av en vektor
+        //  - Metod som vi döper till calcLength
+        //  - Lista med doubles som parameter
+        //  - Returnerar längden --> roten ut (e1² + e2² + ... eN²)
+
+    }
+}
